a65acfdcc90c9f66b33e385ca3205dc02072dbf9
luctst <lucas.tostee@gmail.com>
2020-08-11 14:46:13 +0200

    Switch to http localhost for dev mode

--- api/_utils/responseServer.js
+++ api/_utils/responseServer.js
@@ -13,7 +13,7 @@ module.exports = (response, code, data = {}) => {
   const defaultHeader = {
     "Access-Control-Allow-Origin": `${
       response.remoteDomain === "dev-api-bizzy.vercel.app"
-        ? "https://localhost:3000"
+        ? "http://localhost:3000"
         : "https://bizzy.now.sh"
     }`,
     "Access-Control-Allow-Credentials": true,
