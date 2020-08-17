bf5325347bb589803258782f0cd8bf858235ab27
luctst <lucas.tostee@gmail.com>
2020-08-17 16:12:29 +0200

    Update access-control-allow-origin to localhost

--- api/_utils/responseServer.js
+++ api/_utils/responseServer.js
@@ -13,7 +13,7 @@ module.exports = (response, code, data = {}) => {
   const defaultHeader = {
     "Access-Control-Allow-Origin": `${
       response.remoteDomain === "dev-api-bizzy.vercel.app"
-        ? "https://postwoman.io"
+        ? "http://localhost:3000"
         : "https://bizzy.now.sh"
     }`,
     "Access-Control-Allow-Credentials": true,
