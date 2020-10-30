729f7155ff0c4fc4fb71737aa38cd1c536670970
luctst <lucas.tostee@gmail.com>
2020-10-30 22:34:59 +0100

    Implement monoservice structure

---
+++ lambdas/home.js
@@ -0,0 +1,20 @@
+const response = require("/opt/response");
+
+const routes = {
+  "/forgot": "send mail for resset password.",
+  "/login": "connect an user.",
+  "/logout": "Logout an user.",
+  "/register": "Create an user in bdd.",
+  "/resetpassword": "Reset password user."
+};
+
+/**
+ * Return all routes availables for the Bizzy API.
+ */
+exports.handler = function Home(event, context, cb) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  return cb(null, response(200, {
+    modifyResponse: { ...routes }
+  }));
+};
